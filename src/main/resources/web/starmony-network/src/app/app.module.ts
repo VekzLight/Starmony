09d76c17e06496bff722e94bcda4834da1ea50ca
VekzLight <dnasystem.vekz@gmail.com>
2021-10-06 12:12:46 -0500

    Añade archivos base de modulo web  (Angular + Material)

---
+++ src/main/resources/web/starmony-network/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/main/resources/web/starmony-network/src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+// Material imports
+import { MatCardModule } from '@angular/material/card';
+
+
+@NgModule({
+  exports: [
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
