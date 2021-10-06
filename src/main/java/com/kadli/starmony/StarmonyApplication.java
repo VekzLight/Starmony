2693bd491321a7dddae68f751fca60ea67d2b097
VekzLight <dnasystem.vekz@gmail.com>
2021-10-06 01:53:33 -0500

    Añade proyecto base (SpringBoot + Maven)

---
+++ src/main/java/com/kadli/starmony/StarmonyApplication.java
@@ -0,0 +1,13 @@
+package com.kadli.starmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarmonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarmonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadli/starmony/StarmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadli.starmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarmonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
